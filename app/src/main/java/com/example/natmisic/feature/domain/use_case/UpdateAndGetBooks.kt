d64d64c5dd9055a41177e3633bc048a451a8e08e
CherifiMi <cherifi154949@gmail.com>
2023-07-17 02:53:56 +0100

    loading covers from cash

--- app/src/main/java/com/example/natmisic/feature/domain/use_case/UpdateAndGetBooks.kt
+++ app/src/main/java/com/example/natmisic/feature/domain/use_case/UpdateAndGetBooks.kt
@@ -1,6 +1,8 @@
 package com.example.natmisic.feature.domain.use_case
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.media.MediaMetadataRetriever
 import android.net.Uri
 import androidx.datastore.core.DataStore
@@ -14,6 +16,8 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.runBlocking
+import java.io.File
+import java.io.FileOutputStream
 
 class UpdateAndGetBooks(
     private val repo: Repository,
@@ -32,9 +36,9 @@ class UpdateAndGetBooks(
         val documentFile = DocumentFile.fromTreeUri(context, uri)
         val files = documentFile?.listFiles()?.filter { it.type?.startsWith("audio/") == true }
 
-        files?.forEach {file->
+        files?.forEach { file ->
             val book = fileToBook(file, context)
-            if (repo.getBooks().first().none { it.name == book.name }){
+            if (repo.getBooks().first().none { it.name == book.name }) {
                 repo.insertBook(book)
             }
         }
@@ -46,20 +50,35 @@ class UpdateAndGetBooks(
         val mediaMetadataRetriever = MediaMetadataRetriever()
         mediaMetadataRetriever.setDataSource(context, file.uri)
         val name = mediaMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE)
-        val author = mediaMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST)
-        val duration = mediaMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)
-        val cover = mediaMetadataRetriever.embeddedPicture
-
-        // TODO: convert cover to path
+        val author =
+            mediaMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST)
+        val duration =
+            mediaMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)
+        val cover = coverToPath(mediaMetadataRetriever.embeddedPicture, name ?: "", context)
 
         return Book(
-         path = file.uri.lastPathSegment!!,
-         name = name ?: "",
-         author = author ?: "",
-         cover = "",
-         duration = duration?.toInt() ?: 0,
-         progress = 0,
-         timestamp = emptyList<Timestamp>()
+            path = file.uri.lastPathSegment!!,
+            name = name ?: "",
+            author = author ?: "",
+            cover = cover ?: "",
+            duration = duration?.toInt() ?: 0,
+            progress = 0,
+            timestamp = emptyList<Timestamp>()
         )
+    }
+
+    fun coverToPath(byteArray: ByteArray?, name: String, context: Context): String? {
+        if (byteArray != null) {
+            val bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
+
+            val file = File(context.cacheDir, "$name.jpg")
+            val outputStream = FileOutputStream(file)
+
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+            outputStream.flush()
+            outputStream.close()
+            return file.path
+        }
+        return null
     }
 }
--- app/src/main/java/com/example/natmisic/feature/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/example/natmisic/feature/presentation/home/HomeScreen.kt
@@ -1,20 +1,33 @@
 package com.example.natmisic.feature.presentation.home
 
+import android.graphics.BitmapFactory
 import android.os.Build
+import android.widget.ImageView
 import androidx.annotation.RequiresApi
 import androidx.compose.animation.*
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
 import com.example.natmisic.feature.presentation.home.components.LoadingBall
+import java.io.File
 
 @OptIn(ExperimentalAnimationApi::class)
 @RequiresApi(Build.VERSION_CODES.O)
@@ -47,7 +60,10 @@ fun HomeScreen(
     ) {
         LazyColumn(Modifier.fillMaxSize()) {
             items(state.books) { book ->
-                Text(text = book.name)
+                Box(modifier = Modifier.size(200.dp).padding(40.dp), contentAlignment = Alignment.Center){
+                    Image(painter = rememberAsyncImagePainter(model = File(book.cover)), contentDescription = "")
+                    Text(text = book.name)
+                }
             }
         }
     }
